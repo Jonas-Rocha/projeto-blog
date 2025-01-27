469b49e694dd1686de158aea39fad0a5385c0691
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-01-27 12:35:59 -0300

    segundo dia aprendendo git 01

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,6 @@ export function App() {
           <Post />
           <Post />
           <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
         </main>
       </div>
     </>
